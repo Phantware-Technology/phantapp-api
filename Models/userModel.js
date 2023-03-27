bf49ecda2874e76122fff4e0225f678dcb6987b4
phantware <phantmoney2011@gmail.com>
2023-03-27 18:11:34 +0100

    userModel file added

---
+++ Models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true, minlength: 3, maxlength: 30 },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      minlength: 3,
+      maxlength: 200,
+    },
+    password: { type: String, minlength: 3, maxlength: 1024, required: true },
+  },
+  { timestamps: true }
+)
+
+const userModel = mongoose.model('User', UserSchema)
+module.export = userModel
